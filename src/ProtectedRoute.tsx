7a90992ebb54f32c8c0347bdb9e78182ebb423a3
Dhirajchaudari <dhirajchaudhari789@gmail.com>
2024-06-01 17:11:09 +0530

    fix:protected routes component

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -4,12 +4,12 @@ import { Navigate, Outlet } from "react-router-dom";
 import { getToken } from "./helper";
 import { DashboardPages } from "./interfaces";
 
-interface ProtectedRouteProps {
-  element: React.ReactElement;
-  path: string;
-}
+// interface ProtectedRouteProps {
+//   element: React.ReactElement;
+//   path: string;
+// }
 
-const ProtectedRoute: React.FC<ProtectedRouteProps> = () => {
+const ProtectedRoute: React.FC = () => {
     const isAuthenticated = getToken() !== null;
     console.log(isAuthenticated)
 
